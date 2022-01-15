d73a4d0f1cc4fb7fb92c8d8f6744f50af203e0f6
biku1998 <kumarsourabh228@gmail.com>
2022-01-15 22:33:52 +0530

    added storybook@next for vite support

---
+++ src/components/Button/Button.stories.tsx
@@ -0,0 +1,8 @@
+import Button from ".";
+
+export default {
+  title: "components/Button",
+  component: Button,
+};
+
+export const defaultButton = () => <Button />;
---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,9 @@
+export default function Button() {
+  return (
+    <div>
+      <span className="border border-slate-300 p-2 text-md">
+        Button component
+      </span>
+    </div>
+  );
+}
